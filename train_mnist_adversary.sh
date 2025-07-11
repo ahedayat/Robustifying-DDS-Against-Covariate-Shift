8cee43ac4787b20d6de78e90d7058c208b2d3ae4
ahedayat <alihedayatnia@ut.ac.ir>
2025-07-11 12:51:23 +0330

    adding files for mnist experiments

---
+++ train_mnist_adversary.sh
@@ -0,0 +1,3 @@
+python ./train_mnist_adversary_sigma_25.py
+python ./train_mnist_adversary_sigma_50.py
+python ./train_mnist_adversary_sigma_100.py
---
+++ train_mnist_adversary_sigma_25.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+
+from runners import MNISTTrainerRunner
+
+
+def _main():
+    print("train_cifar10.py")
+    from config.classification.mnist_adversary_sigma_25.data_config import data_config
+    from config.classification.mnist_adversary_sigma_25.classifier_config import classifier_config
+    from config.classification.mnist_adversary_sigma_25.data_config import data_config
+    from config.classification.mnist_adversary_sigma_25.diffusion_config import diffusion_config
+    from config.classification.mnist_adversary_sigma_25.train_config import train_config
+
+    mnist_runner = MNISTTrainerRunner(
+        data_config = data_config,
+        diffusion_config = diffusion_config,
+        classifier_config = classifier_config,
+        train_config = train_config,
+        device = train_config.device
+    )
+
+    mnist_runner.train()
+
+
+if __name__ == "__main__":
+    _main()
