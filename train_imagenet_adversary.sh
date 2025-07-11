cbe5f117af034cb686fbd87408648542ec1d6225
ahedayat <alihedayatnia@ut.ac.ir>
2025-07-11 12:52:41 +0330

    adding files for imagenet experiments

---
+++ train_imagenet_adversary.sh
@@ -0,0 +1,2 @@
+torchrun --nproc_per_node=2 ./train_imagenet_adversary_sigma_50.py
+torchrun --nproc_per_node=2 ./train_imagenet_adversary_sigma_100.py
---
+++ train_imagenet_adversary_sigma_50.py
@@ -0,0 +1,34 @@
+ import os
+import shutil
+
+# from runners import TrainerRunner_v2
+# from runners import MultiGPUTrainerRunner_v2 as TrainerRunner_v2
+from runners import MultiGPUTrainerRunner_v6 as TrainerRunner_v2
+# from runners import MultiGPUTrainerRunner_v5 as TrainerRunner_v2
+
+
+def _main():
+    print("train_cifar10.py")
+    from config.classification.imagenet_adversary_sigma_50.data_config import data_config
+    from config.classification.imagenet_adversary_sigma_50.classifier_config import classifier_config
+    from config.classification.imagenet_adversary_sigma_50.data_config import data_config
+    from config.classification.imagenet_adversary_sigma_50.diffusion_config import diffusion_config
+    from config.classification.imagenet_adversary_sigma_50.train_config import train_config
+    from config.classification.imagenet_adversary_sigma_50.unet_config import unet_config
+
+    # shutil.copytree("./config/classification/cifar10_sigma_100", os.path.join(train_config.log.path, "configs"))
+
+    cifar_runner = TrainerRunner_v2(
+        data_config = data_config,
+        diffusion_config = diffusion_config,
+        unet_config = unet_config,
+        classifier_config = classifier_config,
+        train_config = train_config,
+        device = train_config.device
+    )
+
+    cifar_runner.train()
+
+
+if __name__ == "__main__":
+    _main()
